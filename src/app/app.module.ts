1e0bae1a0822e893e53f9ccc17ac69f84302a59f
just born <justborney@gmail.com>
2022-10-14 16:45:54 +0500

    1:38:00

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { LayoutComponent } from './components/ui/layout/layout.component'
+import { LayoutComponent } from './components/ui/layout/layout.component';
+import { HeaderComponent } from './components/ui/layout/header/header.component'
 
 @NgModule({
-  declarations: [AppComponent, LayoutComponent],
+  declarations: [AppComponent, LayoutComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
